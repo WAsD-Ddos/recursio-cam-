91de8d5221acceb3c855fbc361f409b4f095961b
Дима Д <mitja3375@gmail.com>
2025-04-14 17:49:09 +0300

    Add project files.

---
+++ logic.cpp
@@ -0,0 +1,27 @@
+// [The Count of Even Digits]
+// Количество чётных цифр 
+//
+// Необходимо разработать рекурсивную функцию, которая подсчитывает 
+// количество чётных цифр у заданного пользователем целого числа.
+// Число может быть как положительным, так и отрицательным.
+// Не забудьте, что цифра ноль в математике является чётным числом. 
+
+#include "logic.h"
+
+int count_even_digits(long long number) {
+    if (number < 0) { number = -number; }
+
+
+    if (number < 10) {
+        return (number % 2 == 0) ? 1 : 0;
+    }
+
+
+    int lastDigit = number % 10;
+    if (lastDigit % 2 == 0) {
+        return 1 + count_even_digits(number / 10);
+    }
+    else {
+        return count_even_digits(number / 10);
+    }
+}
---
+++ util.h
@@ -0,0 +1,4 @@
+#include <Windows.h>
+#include "logic.h"
+
+void print_test(long long number, int expected, string test_name);
